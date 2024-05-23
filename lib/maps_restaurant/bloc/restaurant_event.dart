fa88f2cdf46cba96ffe286c77a08b0fc7ab652ec
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2024-05-23 13:49:53 -0400

    Changed props to states instead of event

--- lib/maps_restaurant/bloc/restaurant_event.dart
+++ lib/maps_restaurant/bloc/restaurant_event.dart
@@ -2,10 +2,4 @@ class RestaurantEvent {}
 
 class RestaurantReview extends RestaurantEvent {}
 
-class RestaurantFavorite extends RestaurantEvent {
-  final String id_restaurant;
-
-  RestaurantFavorite({required this.id_restaurant});
-
-  List<Object> get props => [id_restaurant];
-}
+class RestaurantFavorite extends RestaurantEvent {}
--- lib/maps_restaurant/bloc/restaurant_state.dart
+++ lib/maps_restaurant/bloc/restaurant_state.dart
@@ -2,4 +2,10 @@ class RestaurantState {}
 
 class RestaurantLoading extends RestaurantState {}
 
-class RestaurantLoaded extends RestaurantState {}
+class RestaurantLoaded extends RestaurantState {
+  final String id_restaurant;
+
+  RestaurantLoaded({required this.id_restaurant});
+
+  List<Object> get props => [id_restaurant];
+}
